e5d8dbe180e9ea7a49aa3dab4bce26113a7ec659
juanifiore <clinux01@pc51.lab1106>
2023-03-28 15:40:48 -0300

    clase-3

---
+++ clase-3.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+'''
+def pisar_elemento(M,e):
+    for i in range len(M):
+        for j in range len(M[0]):
+            if M[i][j] == e:
+                M[i][j] == -1
+    return M
+'''
+
+a = np.array([1,2,3],[4,5,6])
+print(a.size())
