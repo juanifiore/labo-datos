718d083fcd1c7b43bbc1c43e943f2db5f60d7f06
juanifiore <juanifiore291@gmail.com>
2023-04-16 17:04:51 -0300

    sql guia terminada

--- sql-guia.py
+++ sql-guia.py
@@ -9,13 +9,15 @@ import pandas as pd
 import os
 
 def main():
+
     '''ARMO LOS CSV'''
-    ruta = '/home/juani/Documentos/Facultad/3 LABORATORIO DE DATOS/labo-datos/csv'
+    #ruta = '/home/juani/Documentos/Facultad/3 LABORATORIO DE DATOS/labo-datos/csv'
+    ruta = './csv'
     casos = pd.read_csv(os.path.join(ruta,'casos.csv'))
-    tipo_evento = pd.read_csv(os.path.join(ruta,'tipoevento.csv'))
+    tipoevento = pd.read_csv(os.path.join(ruta,'tipoevento.csv'))
     departamento = pd.read_csv(os.path.join(ruta,'departamento.csv'))
     provincia = pd.read_csv(os.path.join(ruta,'provincia.csv'))
-    grupo_etario = pd.read_csv(os.path.join(ruta,'grupoetario.csv'))
+    grupoetario = pd.read_csv(os.path.join(ruta,'grupoetario.csv'))
 
     ''' ENTRADA PARA ELEGIR EJERCICIO A IMPRIMIR '''
 
@@ -127,10 +129,9 @@ def main():
         consigna = 'Listar los registros de la tabla departamento cuyo codigo de provincia es 54'
 
         consultaSQL = '''
-                        SELECT descripcion, id_provincia
+                        SELECT DISTINCT descripcion, id_provincia
                         FROM departamento
-                        GROUP BY id_provincia
-                        HAVING id_provincia = 54
+                        WHERE id_provincia = '54'
                       '''
 
         imprimirEjercicio(consigna,[departamento],consultaSQL)
@@ -138,17 +139,16 @@ def main():
 
 #---------------------------------------------------------------------------------
 
-    if entrada == '1g':
+    if entrada == '1h':
 
-        '''EJERCICIO 1G'''
+        '''EJERCICIO 1H'''
 
-        consigna = 'Listar los registros de la tabla departamento cuyo codigo de provincia es 54'
+        consigna = 'Listar los registros de la tabla departamento cuyo código de provincia es igual a 22, 78 u 86'
 
         consultaSQL = '''
                         SELECT descripcion, id_provincia
                         FROM departamento
-                        GROUP BY id_provincia
-                        HAVING id_provincia = 54
+                        WHERE id_provincia IN ('22','78','86')
                       '''
 
         imprimirEjercicio(consigna,[departamento],consultaSQL)
@@ -157,6 +157,326 @@ def main():
 
 #---------------------------------------------------------------------------------
 
+
+
+    if entrada == '1i':
+
+        '''EJERCICIO 1I'''
+
+        consigna = 'Listar los registros de la tabla departamento cuyos códigos de provincia se encuentren entre el 50 y el 59 (ambos valores inclusive).'
+
+        consultaSQL = '''
+                        SELECT descripcion, id_provincia
+                        FROM departamento
+                        WHERE id_provincia >= '50' AND id_provincia <= 59
+                      '''
+
+        imprimirEjercicio(consigna,[departamento, provincia],consultaSQL)
+
+
+
+#---------------------------------------------------------------------------------
+
+
+
+    if entrada == '1j':
+
+        '''EJERCICIO 1J'''
+
+        consigna = 'Listar los registros de la tabla provincia cuyos nombres comiencen con la letra M.'
+
+        consultaSQL = '''
+                        SELECT id, descripcion
+                        FROM provincia
+                        WHERE descripcion LIKE 'M%'
+                      '''
+
+        imprimirEjercicio(consigna,[provincia],consultaSQL)
+
+
+
+#---------------------------------------------------------------------------------
+
+
+
+    if entrada == '1k':
+
+        '''EJERCICIO 1K'''
+
+        consigna = 'Listar los registros de la tabla provincia cuyos nombres comiencen con la letra S y su quinta letra sea una letra A.'
+
+        consultaSQL = '''
+                        SELECT id, descripcion
+                        FROM provincia
+                        WHERE descripcion LIKE 'S___a%'
+                      '''
+
+        imprimirEjercicio(consigna,[provincia],consultaSQL)
+
+
+
+#---------------------------------------------------------------------------------
+
+
+
+    if entrada == '1l':
+
+        '''EJERCICIO 1L'''
+
+        consigna = 'Listar provincias que terminan con A'
+
+        consultaSQL = '''
+                        SELECT id, descripcion
+                        FROM provincia
+                        WHERE descripcion LIKE '%a'
+                      '''
+
+        imprimirEjercicio(consigna,[provincia],consultaSQL)
+
+
+
+#---------------------------------------------------------------------------------
+
+
+
+    if entrada == '1m':
+
+        '''EJERCICIO 1M'''
+
+        consigna = 'Provincias con 5 letras'
+
+        consultaSQL = '''
+                        SELECT id, descripcion
+                        FROM provincia
+                        WHERE descripcion LIKE '_____'
+                      '''
+
+        imprimirEjercicio(consigna,[provincia],consultaSQL)
+
+
+
+
+#---------------------------------------------------------------------------------
+
+
+
+    if entrada == '1n':
+
+        '''EJERCICIO 1N'''
+
+        consigna = 'Provincias que tengan do en alguna parte de su nombre'
+
+        consultaSQL = '''
+                        SELECT id, descripcion
+                        FROM provincia
+                        WHERE descripcion LIKE '%do%'
+                      '''
+
+        imprimirEjercicio(consigna,[provincia],consultaSQL)
+
+
+
+#---------------------------------------------------------------------------------
+
+
+
+    if entrada == '1o':
+
+        '''EJERCICIO 1O'''
+
+        consigna = 'Provincias que tengan do y su id sea menor a 30'
+
+        consultaSQL = '''
+                        SELECT id, descripcion
+                        FROM provincia
+                        WHERE descripcion LIKE '%do%' AND id < '30'
+                      '''
+
+        imprimirEjercicio(consigna,[provincia],consultaSQL)
+
+
+
+#---------------------------------------------------------------------------------
+
+
+
+    if entrada == '1p':
+
+        '''EJERCICIO 1P'''
+
+        consigna = 'Listar los registros de la tabla departamento cuyos nombres tengan ”san” en alguna parte de su nombre. Listar sólo id y descripcion. Utilizar los siguientes alias para las columnas: codigo_depto y nombre_depto, respectivamente. El resultado debe estar ordenado por sus nombres de manera descendentes (de la Z a la A).'
+
+        consultaSQL = '''
+                        SELECT id AS codigo_depto, descripcion AS nombre_depto
+                        FROM departamento
+                        WHERE nombre_depto LIKE '%pa%'
+                        ORDER BY nombre_depto DESC
+                      '''
+
+        imprimirEjercicio(consigna,[departamento],consultaSQL)
+
+
+
+#---------------------------------------------------------------------------------
+
+# INNER JOIN
+
+    if entrada == '2a':
+
+        '''EJERCICIO 2A'''
+
+        consigna = 'Devolver una lista con los código y nombres de departamentos, asociados al nombre de la provincia al que pertenecen'
+
+        consultaSQL = '''
+                        SELECT departamento.id , departamento.descripcion AS nombre_depto, provincia.descripcion AS provincia
+                        FROM departamento
+                        INNER JOIN provincia ON provincia.id = id_provincia
+                      '''
+
+        imprimirEjercicio(consigna,[departamento,provincia],consultaSQL)
+
+
+
+#---------------------------------------------------------------------------------
+
+
+    if entrada == '2b':
+
+        '''EJERCICIO 2B'''
+
+        consigna = 'Devolver una lista con los código y nombres de departamentos, asociados al nombre de la provincia al que pertenecen. Ordenar el resultado por nombre de provincia de manera ascendente, y dentro de cada una de ellas por nombre de departamento, también de manera ascendente.'
+
+        consultaSQL = '''
+                        SELECT departamento.id , departamento.descripcion AS nombre_depto, provincia.descripcion AS provincia
+                        FROM departamento
+                        INNER JOIN provincia ON provincia.id = id_provincia
+                        ORDER BY provincia ASC, nombre_depto ASC 
+                      '''
+
+        imprimirEjercicio(consigna,[departamento,provincia],consultaSQL)
+
+
+
+#---------------------------------------------------------------------------------
+
+
+    if entrada == '2c':
+
+        '''EJERCICIO 2C'''
+
+        consigna = 'Devolver los casos de la provincia de Chaco'
+
+        consultaSQL = '''
+                        SELECT casos.*, provincia.descripcion AS provincia 
+                        FROM casos, departamento, provincia
+                        WHERE id_depto = departamento.id AND id_provincia = provincia.id AND provincia.descripcion = 'Chaco'
+                      '''
+
+        imprimirEjercicio(consigna,[casos,provincia],consultaSQL)
+
+
+
+#---------------------------------------------------------------------------------
+
+
+    if entrada == '2d':
+
+        '''EJERCICIO 2D'''
+
+        consigna = 'Devolver casos de BA cuyo campo cantidad supere los 10 casos'
+
+        consultaSQL = '''
+                        SELECT DISTINCT casos.*, provincia.descripcion AS provincia 
+                        FROM casos, departamento, provincia
+                        WHERE id_depto = departamento.id AND id_provincia = provincia.id AND provincia.descripcion = 'Buenos Aires' AND cantidad > '10'
+                      '''
+
+        imprimirEjercicio(consigna,[casos,provincia],consultaSQL)
+            
+
+
+#---------------------------------------------------------------------------------
+
+
+    if entrada == '2e':
+
+        '''EJERCICIO 2E'''
+
+        consigna = 'Devolver aquellos casos de las provincias cuyo nombre terminen con la letra a y el campo cantidad supere 10. Mostrar: nombre de provincia, nombre de departamento, año, semana epidemiológica, descripción de grupo etario y cantidad. Ordenar el resultado por la cantidad (descendente), luego por el nombre de la provincia (ascendente), nombre del departamento (ascendente), año (ascendente) y la descripción del grupo etario (ascendente)'         
+
+        consultaSQL = '''
+                        SELECT provincia.descripcion AS provincia, departamento.descripcion AS depto, anio, semana_epidemiologica, grupoetario.descripcion AS grupoEtario, cantidad
+                        FROM casos, departamento, provincia, grupoetario
+                        WHERE id_grupoetario = grupoetario.id AND id_depto = departamento.id AND id_provincia = provincia.id AND provincia.descripcion LIKE '%a' AND cantidad > '10'
+                        ORDER BY cantidad DESC, provincia ASC, departamento ASC, anio ASC, grupoEtario ASC
+                      '''
+
+        imprimirEjercicio(consigna,[casos,provincia],consultaSQL)
+
+
+
+
+
+#---------------------------------------------------------------------------------
+
+
+    if entrada == '2f':
+
+        '''EJERCICIO 2F'''
+
+        consigna = 'Idem anterior pero solo aquellas tuplas que tengan el maximo en el campo cantidad'
+
+        consultaSQL = '''
+                        SELECT provincia.descripcion AS provincia, departamento.descripcion AS depto, anio, semana_epidemiologica, grupoetario.descripcion AS grupoEtario, MAX(cantidad) AS cant_max
+                        FROM casos, departamento, provincia, grupoetario
+                        WHERE id_grupoetario = grupoetario.id AND id_depto = departamento.id AND id_provincia = provincia.id AND provincia.descripcion LIKE '%a' AND cantidad > '10'
+                        GROUP BY * 
+                        HAVING cantidad = 'cant_max'
+                        ORDER BY cantidad DESC, provincia ASC, departamento ASC, anio ASC, grupoEtario ASC
+                      '''
+
+        imprimirEjercicio(consigna,[casos,provincia],consultaSQL)
+
+
+
+#-------------------------------------------------------------------------------------
+
+# OUTER JOIN 
+
+    if entrada == '3a':
+
+        '''EJERCICCIO 3A'''
+
+        consigna = 'Devolver un listado con los nombres de los deptos qu no tienen casos asociados'
+
+        consultaSQL = '''
+                        SELECT departamento.descripcion AS nombre_depto_sin_casos
+                        FROM departamento
+                        LEFT OUTER JOIN casos ON id_depto = departamento.id
+                        WHERE casos.id IS NULL
+                      '''
+
+        imprimirEjercicio(consigna,[casos,departamento],consultaSQL)
+
+
+
+#-------------------------------------------------------------------------------------
+
+
+    if entrada == '3b':
+
+        '''EJERCICCIO 3B'''
+
+        consigna = 'Devolver un listado con los tipo evento que no tienen casos asociados'
+
+        consultaSQL = '''
+                        SELECT DISTINCT tipoevento.descripcion AS tipoevento_sin_casos
+                        FROM tipoevento
+                        LEFT OUTER JOIN casos ON id_tipoevento = tipoevento.id
+                        WHERE casos.id IS NULL
+                      '''
+
+        imprimirEjercicio(consigna,[casos,tipoevento],consultaSQL)
 
 
 
