fc54a38eb16eb6118dcadadb6b3e222f7e749d3e
juanifiore <clinux01@pc66.lab1103>
2023-05-17 11:55:23 -0300

    ru-id

--- ru-di.py
+++ ru-di.py
@@ -2,18 +2,41 @@ import pandas as pd
 from sklearn import linear_model
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('ru-di.csv')
-
-#plt.plot(x = df['RU'],y=df['DI'])
+#df = pd.read_csv('./csv/ru-di.csv')
+#
+##plt.plot(x = df['RU'],y=df['DI'])
+##plt.show()
+##plt.close()
+#
+#x = pd.DataFrame(df['RU'])
+#y = pd.DataFrame(df['DI'])
+#
+#model = linear_model.LinearRegression()
+#
+#model.fit(x,y)
+#
+#plt.scatter(x,y)
+#
+#plt.plot(x,model.predict(x))
+#
 #plt.show()
-#plt.close()
+
+df = pd.read_csv('./csv/datos_libreta_25922.csv')
 
 x = pd.DataFrame(df['RU'])
-y = pd.DataFrame(df['DI'])
+y = pd.DataFrame(df['ID'])
 
 model = linear_model.LinearRegression()
 
 model.fit(x,y)
+print('DF =')
+print(df)
+print()
+print('Coref = ',model.coef_)
+print()
+print('Intercept = ',model.intercept_)
+print()
+print('R2 = ',model.score(x,y))
 
 plt.scatter(x,y)
 
